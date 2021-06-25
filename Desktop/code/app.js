f52722427caa56cdc7e27a0bfff334f66f7e07e9
MikaGasp <85709612+MikaGasp@users.noreply.github.com>
2021-06-25 04:57:39 +0400

    Update app.js

--- Desktop/code/app.js
+++ Desktop/code/app.js
@@ -3,18 +3,18 @@
 function security(email, pass) {
     const regV = /@gmail.com/g;
     const result = email.match(regV);
+    if (pass.length <= 15){
+        const regE = /(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9!@#$%^&*a-zA-Z]{3}/g
+        const result1 = pass.match(regE);
     if(result){
         console.log(email);
     } else {
         console.log('sxala');
     }
-    if (pass.length <= 15){
-        const regE = /(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9!@#$%^&*a-zA-Z]{3}/g
-        const result1 = pass.match(regE);
-        if(result1){
-            console.log(pass);
-        }else{
-            console.log('sxala');
+    if(result1){
+        console.log(pass);
+    }else{
+        console.log('sxala');
         }
     }
 }
