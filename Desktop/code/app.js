ccb0bd395302539be1dcd95f80ff4d4fdb7c4c3e
MikaGasp <85709612+MikaGasp@users.noreply.github.com>
2021-06-28 13:41:15 +0400

    Update app.js

--- Desktop/code/app.js
+++ Desktop/code/app.js
@@ -12,6 +12,13 @@ class Cars {
 }
 const cars = new Cars('lada', 'niva', 'white', 1.7, 137);
 cars.interior.push('White');
-cars.equipment = ('Full');
-cars.fuel.push = ('50%');
+cars.equipment.push('Full');
+cars.fuel.push('50%');
 console.log(cars);
+
+
+const cars1 = new Cars('bmw','x6','black', 3.0, 350);
+cars1.interior.push('Black');
+cars1.equipment.push('Standart');
+cars1.fuel.push('66%')
+console.log(cars1);
