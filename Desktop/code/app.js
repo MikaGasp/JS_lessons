309f2f44ca906f99fe6d8ef78ea2196cfebf4978
MikaGasp <85709612+MikaGasp@users.noreply.github.com>
2021-06-30 14:49:14 +0400

    Update app.js

--- Desktop/code/app.js
+++ Desktop/code/app.js
@@ -1,24 +1,102 @@
-class Cars {
-    constructor(brand, model, color, engine, power){
-        this.brand = brand;
-        this.model = model;
-        this.color = color;
-        this.engine = engine;
-        this.power = power;
-    }
-    interior = [];
-    equipment = [];
-    fuel = [];
+
+//1.
+const c = {
+    name: 'Mika '
 }
-const cars = new Cars('lada', 'niva', 'white', 1.7, 137);
-cars.interior.push('White');
-cars.equipment.push('Full');
-cars.fuel.push('50%');
-console.log(cars);
+
+function D(){}
+D.prototype = c;
+const e = new D;
 
 
-const cars1 = new Cars('bmw','x6','black', 3.0, 350);
-cars1.interior.push('Black');
-cars1.equipment.push('Standart');
-cars1.fuel.push('66%')
-console.log(cars1);
+//2.
+const t = {
+    lastName: 'Gasparyan '
+}
+
+function F(){}
+F.prototype = t;
+const a = new F;
+
+
+//3.
+const b = {
+    age: 19
+}
+
+function U(){}
+U.prototype = b;
+const r = new U;
+
+
+
+//4. 
+
+const w = {
+    studing: ' studing Basic it centre '
+}
+function Q(){}
+Q.prototype = w;
+const p = new Q;
+
+
+
+//5.
+
+const j = {
+    activity: 'activity 4, '
+}
+
+function H(){}
+H.prototype = j;
+const k = new H;
+
+
+//6.
+const s = {
+    absence: 'absence 2, '
+}
+
+function Z(){}
+Z.prototype = s;
+const x = new Z;
+
+
+//7.
+const v = {
+    homework: 'Homework 3, '
+}
+
+function N(){}
+N.prototype = v;
+const m = new N;
+
+
+
+//8.
+const f1 = {
+    reprimand: 'reprimand 999, '
+}
+function W1(){}
+W1.prototype = f1;
+const a1 = new W1;
+
+
+//9.
+const b1 = {
+    vacation: 'vacation 2 week, '
+}
+function E1() {}
+E1.prototype = b1;
+const t1 = new E1;
+
+
+const y1 = {
+    phone: 'phone number - (094)457330'
+}
+function L1(){}
+L1.prototype = y1;
+const m1 = new L1;
+
+
+console.log(e.name + a.lastName + r.age + p.studing + k.activity + x.absence + m.homework + a1.reprimand + t1.vacation + m1.phone);
