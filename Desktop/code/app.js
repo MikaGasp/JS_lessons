c11676ea1977b1a857fc019c13d7f78321b62e4b
MikaGasp <85709612+MikaGasp@users.noreply.github.com>
2021-07-21 16:19:41 +0400

    Update app.js

--- Desktop/code/app.js
+++ Desktop/code/app.js
@@ -1,113 +1,74 @@
+const e = require('express');
 const express = require('express');
-
 const app = express();
 
-app.get('/student/1', (req,res) => {
-    res.send({
-        name: 'Mika'
-    })
-});
+app.use(express.json({ limit: '50mb '}));
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/student/2', (req,res) => {
-    res.send({
-        name: 'Edgar'
-    })
-});
+class Error {
+    constructor(){};
+    userNotFound(){
+        throw new Error('User not a found!')
+    }
+}
 
-app.get('/student/3', (req,res) => {
-    res.send({
-        name: 'Hasmik'
-    })
-});
+const error = new Error();
 
-app.get('/student/4', (req,res) => {
-    res.send({
-        name: 'Eduard'
-    })
-});
+const students = [];
 
-app.get('/students', (req,res) => {
-    res.send({
-        names: ['Mika', 'Edgar', 'Hasmik', 'Eduard']
-    })
+app.post('/student', (req, res) =>{
+
+    students.push({
+        name: req.body.name,
+        age: req.body.age,
+        email: req.body.email,
+        phone: req.body.phone,
+    });
+
+        res.send('success')
 })
 
-app.get('/student/age/1', (req,res) => {
-    res.send({
-        age: 19
-    })
-});
-
-app.get('/student/age/2', (req,res) => {
-    res.send({
-        age: 27
-    })
-});
-
-app.get('/student/age/3', (req,res) => {
-    res.send({
-        age: 28
-    })
-});
-
-app.get('/student/age/4', (req,res) => {
-    res.send({
-        age: 18
-    })
-});
-
-app.get('/student/email/1', (req,res) => {
-    res.send({
-        email: 'mika@mail.ru'
-    })
-});
-
-app.get('/student/email/2', (req,res) => {
-    res.send({
-        email: 'edgar@mail.ru'
-    })
-});
-
-app.get('/student/email/3', (req,res) => {
-    res.send({
-        email: 'Hasmik@mail.ru'
-    })
-});
-
-app.get('/student/email/4', (req,res) => {
-    res.send({
-        email: 'eduard@mail.ru'
-    })
-});
-
-app.get('/student/emails', (req,res) => {
-    res.send({
-        email: ['mika@mail.ru', 'edgar@mail.ru', 'hasmik@mail.ru', 'eduard@mail.ru']
-    })
-});
-
-app.get('/student/activity/1', (req,res) => {
-    res.send({
-        activity: [3, 2, 3, 2, 2]
-    })
+app.get('/student', (req, res) => {
+    res.send(students);
 })
 
-app.get('/student/activity/2', (req,res) => {
-    res.send({
-        activity: [9, 9, 8, 9, 9]
-    })
+app.get('/student/:searchBy/:value', (req, res) => {
+    for(let i = 0; i < students.length; ++i){
+        console.log(req);
+        if(students[i][req.params.searchBy] === req.params.value){
+            return res.send(students[i]);
+        }
+    }
+    return error.userNotFound();
+    //return res.send('student not found!');
 })
 
-app.get('/student/activity/3', (req,res) => {
-    res.send({
-        activity: [9, 9, 9, 9, 8]
-    })
+app.delete('/student/:deleteBy/:value', (req, res) => {
+    for(let i = 0; i < students.length; ++i) {
+        if(students[i][req.params.deleteBy] === req.params.value){
+            delete students[i];
+            return res.send('success');
+        }
+    } 
+    return error.userNotFound();    
+    //return res.send('student not found!');
+    
+
 })
 
-app.get('/student/activity/4', (req,res) => {
-    res.send({
-        activity: [2, 2, 3, 3, 2]
-    })
-})
+app.put('/student/:searchByValue', (req, res) => {
+    for(let i = 0; i < students.length; ++i){
+        if(students[i].email === req.params.searchByValue){
+            for(let key in req.body){
+                students[i][key] = req.body[key];
+                   return res.send('Changes has been done')
+            }
+        } else{
+            return error.userNotFound();
+            //return res.send('Student not found!')
+        }
+    }
+});
 
-app.listen(3000);
+
+app.listen(3000);   
