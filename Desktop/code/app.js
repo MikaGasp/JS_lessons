4cd22817a5b5b1ae7c776822787413393285d514
MikaGasp <85709612+MikaGasp@users.noreply.github.com>
2021-08-08 16:47:42 +0400

    Update app.js

--- Desktop/code/app.js
+++ Desktop/code/app.js
@@ -1,74 +1,74 @@
-const e = require('express');
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
+//const exphbs = require('express-handlebars');
 
-app.use(express.json({ limit: '50mb '}));
-app.use(express.urlencoded({ extended: false }));
+const PORT = 3000
 
-class Error {
-    constructor(){};
-    userNotFound(){
-        throw new Error('User not a found!')
+const app = express(); 
+
+
+function start(){  
+    try{
+         mongoose.connect('mongodb+srv://mika:mika123@cluster0.ffkp0.mongodb.net/todos', {
+            useNewUrlParser: true,
+            useFindAndModify: false,
+            useUnifiedTopology: true
+        })
+        app.listen(PORT, () => {
+            console.log('Server has been started...');
+        })
+    } catch(e){
+        console.log(e);
     }
 }
 
-const error = new Error();
+start();
 
-const students = [];
-
-app.post('/student', (req, res) =>{
-
-    students.push({
-        name: req.body.name,
-        age: req.body.age,
-        email: req.body.email,
-        phone: req.body.phone,
-    });
-
-        res.send('success')
-})
-
-app.get('/student', (req, res) => {
-    res.send(students);
-})
-
-app.get('/student/:searchBy/:value', (req, res) => {
-    for(let i = 0; i < students.length; ++i){
-        console.log(req);
-        if(students[i][req.params.searchBy] === req.params.value){
-            return res.send(students[i]);
-        }
-    }
-    return error.userNotFound();
-    //return res.send('student not found!');
-})
-
-app.delete('/student/:deleteBy/:value', (req, res) => {
-    for(let i = 0; i < students.length; ++i) {
-        if(students[i][req.params.deleteBy] === req.params.value){
-            delete students[i];
-            return res.send('success');
-        }
-    } 
-    return error.userNotFound();    
-    //return res.send('student not found!');
-    
-
-})
-
-app.put('/student/:searchByValue', (req, res) => {
-    for(let i = 0; i < students.length; ++i){
-        if(students[i].email === req.params.searchByValue){
-            for(let key in req.body){
-                students[i][key] = req.body[key];
-                   return res.send('Changes has been done')
-            }
-        } else{
-            return error.userNotFound();
-            //return res.send('Student not found!')
-        }
-    }
+const db = mongoose.connection;
+db.once('open', () =>{
+    console.log('Kpav');
 });
 
+const userSchema = new mongoose.Schema({
+    name: String,
+    age: Number
+})
 
-app.listen(3000);   
+const user1 = mongoose.model(
+    'myUser', userSchema
+)
+
+app.use(express.json());
+
+app.post('/users', (req, res) =>{
+
+    const user = new user1({
+        name: req.body.name,
+        age: req.body.age
+    });
+
+    try{
+        user.save()
+        }
+        
+        catch(e){
+            console.log(e);
+        }
+        
+
+        res.send(user);
+})
+
+
+app.get('/users', (req, res) =>{
+    user1.find({}, (err, users) =>{
+        if(err){
+            throw new Error(err.name);
+        } 
+        res.send(users)});
+})
+
+app.delete('/users/:userId', (req, res) =>{
+    user1.findOneAndRemove({id:req.params.userId})
+    res.send('deleted');
+})
